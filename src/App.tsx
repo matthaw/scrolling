1134f8805f2f64eb507972c556f31744fb6aa667
matthaw <hardway3@protonmail.com>
2021-09-14 22:19:42 -0300

    feat: use PostContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
 import { theme } from '@chakra-ui/theme';
 import { useDisclosure } from '@chakra-ui/hooks';
@@ -8,10 +8,26 @@ import { SidebarContext } from './hooks/useSidebar';
 import { Sidebar } from './components/drawer';
 import { Cards } from './components/cards';
 import { useFilter, FilterContext } from './hooks/useFilter';
+import { PostsContext } from './hooks/usePosts';
+import { Posts, request } from './lib/reddit';
 
 export const App = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const { filter, setFilter } = useFilter();
+  const [posts, setPosts] = useState<Posts[]>([]);
+
+  useEffect(() => {
+    async function fetch() {
+      try {
+        const posts = await request({ name: 'gifs', filter });
+        setPosts(posts);
+        console.log(posts);
+      } catch (err) {
+        console.error(err);
+      }
+    }
+    fetch();
+  }, []);
 
   return (
     <ChakraProvider theme={theme}>
@@ -19,7 +35,9 @@ export const App = () => {
         <FilterContext.Provider value={{ filter, setFilter }}>
           <Navbar />
           <Sidebar />
-          <Cards />
+          <PostsContext.Provider value={{ posts, setPosts }}>
+            <Cards />
+          </PostsContext.Provider>
         </FilterContext.Provider>
       </SidebarContext.Provider>
     </ChakraProvider>
