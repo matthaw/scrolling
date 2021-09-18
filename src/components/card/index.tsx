a3debe5b5e61e584a3c8a28e75b6973b582f8545
matthaw <hardway3@protonmail.com>
2021-09-18 10:34:20 -0300

    refactor: remove render media component

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -1,37 +1,16 @@
 import React, { memo } from 'react';
-import {
-  Flex,
-  Box,
-  Image,
-  Skeleton,
-  useColorModeValue,
-  AspectRatio,
-} from '@chakra-ui/react';
+import { Flex, Box, Skeleton, useColorModeValue } from '@chakra-ui/react';
+
+import Media from '../media/index';
 import { Posts } from '../../lib/reddit';
 import { useModal } from '../../hooks/useModal';
 
-interface PostsProps extends Partial<Posts> {}
+interface CardProps {
+  post: Posts;
+}
 
-function Card({ url, permalink, title, reddit, media }: PostsProps) {
+function Card({ post }: CardProps) {
   const { onOpen, setPost } = useModal();
-
-  function renderMedia(url: string, type: string) {
-    if (type === 'video') {
-      return (
-        <video autoPlay={true} loop={true} muted onClick={onOpen}>
-          <source src={url} type="video/mp4"></source>
-        </video>
-      );
-    }
-
-    if (type === 'image') {
-      return (
-        <AspectRatio onClick={onOpen} height="100%" width="100%" ratio={1}>
-          <Image src={url} scrolling="no" width="100%" height="100%" />
-        </AspectRatio>
-      );
-    }
-  }
 
   return (
     <Flex p={2} alignItems="center" justifyContent="center">
@@ -51,11 +30,22 @@ function Card({ url, permalink, title, reddit, media }: PostsProps) {
           transform: 'scale(1.1)',
           _after: {},
         }}
+        onClick={() => {
+          setPost(post);
+        }}
       >
-        <Box width={media?.oembed?.width} height={media?.oembed?.height}>
-          {url && media?.oembed?.type && renderMedia(url, media?.oembed?.type)}
+        <Box width={400}>
+          {post.url &&
+            Media({
+              post,
+              muted: true,
+              autoPlay: true,
+              onOpen,
+              controls: false,
+              loop: true,
+            })}
 
-          {!url && (
+          {!post.url && (
             <Skeleton
               height="100%"
               width="100%"
@@ -68,33 +58,6 @@ function Card({ url, permalink, title, reddit, media }: PostsProps) {
             />
           )}
         </Box>
-
-        {/* <Box p="6">
-          {title && <Text fontWeight="bold">{formateTitle(title)}</Text>}
-          {!title && <Skeleton width="80%" height="20px" />}
-
-          <Flex
-            mt="1"
-            alignContent="center"
-            display="inline-flex"
-            width="200px"
-          >
-            <Text fontSize="sm">
-              Visit post{' '}
-              {reddit && (
-                <Link
-                  ml="1"
-                  color={'orange.400'}
-                  href={`https://www.reddit.com${permalink}`}
-                  target={'_blank'}
-                >
-                  {reddit}
-                </Link>
-              )}
-            </Text>
-            {!reddit && <Skeleton ml="1" width="40%" height="18px" />}
-          </Flex>
-        </Box> */}
       </Box>
     </Flex>
   );
